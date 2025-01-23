34e554b98ca42d50dea7a40635fe51d891b64137
MissTipo <dorine.a.tipo@gmail.com>
2025-01-23 12:37:21 +0300

    Read a blob object

--- src/blob.h
+++ src/blob.h
@@ -5,4 +5,9 @@
 #define CHUNK 16384
 #define OBJ_DIR ".git/objects"
 
+/* Function prototypes */
+void get_file_path(char *file_path, char *object_hash);
+int decompress_blob(FILE *file, unsigned char **blob_data, size_t *blob_size);
+int cat_file(char *fp, char *path);
+
 #endif

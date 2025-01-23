ab5d5e4db86af93585e5ea4a62a683255b3130ed
MissTipo <dorine.a.tipo@gmail.com>
2025-01-23 11:15:46 +0300

    Read a blob object

--- src/blob.c
+++ src/blob.c
@@ -131,12 +131,12 @@ int cat_file(char *fp, char *path) {
   size_t decompressed_size;
   int ret;
 
-  ret = decompress_blob(fp, &decompressed_data, &decompressed_size);
+  ret = decompress_blob(*file, &decompressed_data, &decompressed_size);
   if (ret != Z_OK) {
     return ret;
   }
 
-  ret = extract_and_print_content(decompressed_data, decompressed_size);
+  ret = extract_and_print_content(*decompressed_data, decompressed_size);
   free(decompressed_data);
   return ret;
   fclose(f);
